74c8fcd29c4403a9f721a736e361b5158c2de596
soni prajapati <96530842+soniprajapati@users.noreply.github.com>
2022-06-06 12:21:19 +0530

    Add files via upload

---
+++ l1.js
@@ -0,0 +1,24 @@
+// string
+var Name = "soni";
+console.log("string data type:",Name);
+console.log("string data type:",typeof Name);
+
+// number
+let marks= 34;
+console.log("data:",marks);
+console.log("data type:",typeof marks);
+
+// boolean
+let $=true;
+console.log("data:",$);
+console.log("data type:",typeof $);
+
+// null
+var nullvar=null;
+console.log("data:",nullvar);
+console.log("data type:",typeof nullvar);
+
+// undefined
+let undef=undefined;
+console.log("data:",undef);
+console.log("data type:",typeof undef);
